b45ffd60f8331346f23e5cc9b1fd1c83db85e9ed
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-01-13 00:36:09 +0300

    Starting to upload new files

---
+++ PortalGR1.0.5/includes/php/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>بوابة العلاقات الحكومية</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="https://kit.fontawesome.com/03da71d2d8.js"></script>
+    <link rel="shortcut icon" type="image/png" href="images\favicon.ico"/>
+</head>
+<body>
+<!--Warning It is added to all pages on the site-->
+<h1 class="alertText"><i class="fas fa-exclamation-triangle"></i>
+    </br>المنصة لا تعمل على هذا الحجم من الأجهزة</h1>
+<!--Site logo, title and subtitle-->
+<header>
+    <div class="headerlogo">
+    <div class="headerlogologo"><img src="https://storage.sg.content-cdn.io/in-resources/8e6b2cb0-d95c-421b-9265-715d6b149ba8/Images/userimages/Herfy%20Logo.svg" alt="logo" class="logo"></div>
+    <div class="headerlogologoname"><h1 class="logoname">بوابة العلاقات الحكومية</br><h1 class="sublogoname">فرع المنطقة الجنوبية</h1></h1></div>
+    </div>
+</header>
---
+++ PortalGR1.0.5/includes/php/nav.php
@@ -0,0 +1,16 @@
+<!--You can add more pages through this page-->
+<nav>
+        <ul>
+            <li><a href="index.php"><i class="fas fa-home"></i></br>الرئيسية</a></li>
+            <li><a href="https://forms.gle/hQxVxdhZArbFqshd6"><i class="fas fa-folder-plus"></i></br>تحديث الملفات</a></li>
+            <li><a href="All branches.php"><i class="fas fa-store-alt"></i></br>كافة الفروع</a></li>
+            <li><a href=""><i class="fas fa-ellipsis-h"></i></br>المزيد</a>
+                <ul>
+                    <li><a href="https://cutt.us/Notice"><i class="fas fa-flag"></i></br>البلاغات</a></li>
+                    <li><a href="https://docs.google.com/spreadsheets/d/1I008hGuYYxUvXudvhSeEizcHvnxTEGJyfs11SrD-BAI/edit?usp=sharing"><i class="fas fa-clock"></i></br>الرخص</a></li>
+                    <li><a href="Versions.php"><i class="fas fa-code-branch"></i></br>الإصدارات</a></li>
+                    <li><a href="https://forms.gle/8z1Ktxq6jVUNkdUw6"><i class="fas fa-star"></i></br>قيمنا</a></li>
+                </ul>
+            </li>
+        </ul>
+</nav>
