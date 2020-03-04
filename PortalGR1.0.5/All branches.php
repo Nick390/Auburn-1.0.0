ca2e130501596c4038d8b94522a95542f5e30ab0
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-03-04 15:25:14 +0300

    Rearrange all files
    *Rearrange all files.

--- PortalGR1.0.5/All branches.php
+++
@@ -1,6 +0,0 @@
-<?php include('includes/php/header.php'); ?>
-<?php include('includes/php/nav.php'); ?>
-<article>
-<!--Add the text that you would like to appear on this page-->
-</article>
-<?php include('includes/php/footer.php'); ?>
---
+++ PortalGR1.0.5/pages/All branches.php
@@ -0,0 +1,6 @@
+<?php include('includes/php/header.php'); ?>
+<?php include('includes/php/nav.php'); ?>
+<article>
+<!--Add the text that you would like to appear on this page-->
+</article>
+<?php include('includes/php/footer.php'); ?>
