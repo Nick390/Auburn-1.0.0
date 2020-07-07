1e9215ef1ecc633abe9d5306fb21855f6e3d8a22
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-07-07 14:51:19 +0300

    Add in add-a-new-branch.php a script
    *Add in add-a-new-branch.php a script to load red color if not fill info in the input field.
    *Update in add-a-new-branch.php to contact to sql.
    *Update in index.html to stop autocomplete.

--- pages/add-a-new-branch.php
+++ pages/add-a-new-branch.php
@@ -4,20 +4,19 @@
 //نص الإتصال بقاعدة البيانات
 require_once '../includes/source/dp_connect.php';
 //نص إرسال النموذج
-if(isset($_POST['signup-btn'])) {
+//النموذج الواقعي غير متصل مع النموذج الذي يرسل إلى قاعدة البيانات يحتاج إلى إصلاح
+if(isset($_POST['submit'])) {
 
-      $username = $_POST['user-name'];
-      $email = $_POST['user-email'];
-      $password = $_POST['user-pass'];
-
-      $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+      $JobNumber = $_POST['JobNumber'];
+      $BranchNumber = $_POST['BranchNumber'];
+      $Neighborhood = $_POST['Neighborhood'];
+      $Region = $_POST['Region'];
 
     try {
-      $SQLInsert = "INSERT INTO users (username, password, email, to_date)
-                   VALUES (:username, :password, :email, now())";
+      $SQLInsert = "INSERT INTO `add-a-new-branch` (`ID`, `JobNumber`, `BranchNumber`, `Neighborhood`, `Region`) VALUES (NULL, 'JobNumber', 'BranchNumber', 'Neighborhood', 'Region');";
 
       $statement = $conn->prepare($SQLInsert);
-      $statement->execute(array(':username' => $username, ':password' => $hashed_password, ':email' => $email));
+      $statement->execute(array(':JobNumber' => $JobNumber, ':BranchNumber' => $BranchNumber, ':Neighborhood' => $Neighborhood, ':Region' => $Region));
 
       if($statement->rowCount() == 1) {
         header('location: /index.html');
@@ -38,6 +37,7 @@ if(isset($_POST['signup-btn'])) {
     <li class="breadcrumb-item active" aria-current="page">إضافة فرع</li>
   </ol>
 </nav>
+<!--ملاحظة عند إضافة فرع جديد-->
 <div class="container">
     <h4>إضافة فرع جديد</h4>
     <hr>
@@ -48,32 +48,32 @@ if(isset($_POST['signup-btn'])) {
     </div>
     
 <!--A form to add a new branch-->
-<form class="was-validated">
+<form class="needs-validation" autocomplete="off" novalidate>
     <div class="container">
         <div class="form-group">
             <label>الإسم</label>
-            <input type="text" class="form-control" id="lastName" placeholder="مثل، أحمد الوادعي" value="" required />
+            <input type="text" class="form-control" id="JobNumber" placeholder="مثل، 12345" value="" required />
             <div class="invalid-feedback">
-      ادخل إسمك الثنائي وباللغة العربية
+      ادخل رقمك الوظيفي باللغة الإنجليزيه
     </div>
         </div>
         <div class="form-group">
             <label>رقم الفرع</label>
-            <input type="text" class="form-control" id="lastName" placeholder="مثل، 14، 22" value="" required />
+            <input type="text" class="form-control" id="BranchNumber" placeholder="مثل، 14، 22" value="" required />
             <div class="invalid-feedback">
       ادخل رقم الفرع بدون صفر في البداية
     </div>
         </div>
         <div class="form-group">
             <label>الحي</label>
-            <input type="text" class="form-control" id="lastName" placeholder="مثل، الفتح، النصب" value="" required />
+            <input type="text" class="form-control" id="Neighborhood" placeholder="مثل، الفتح، النصب" value="" required />
             <div class="invalid-feedback">
       ادخل الحي بإسمه الصحيح إذا كان الإسم يحتوي على كسور او همزه قم بكتابة حالتين للفرع مثل النصب/ألنصب
     </div>
         </div>
         <div class="form-group">
             <label>المنطقة</label>
-            <input type="text" class="form-control" id="lastName" placeholder="مثل، خميس مشيط،ابها" value="" required />
+            <input type="text" class="form-control" id="Region" placeholder="مثل، خميس مشيط،ابها" value="" required />
             <div class="invalid-feedback">
             ادخل المنطقة بإسمه الصحيح إذا كان الإسم يحتوي على كسور او همزه قم بكتابة حالتين للفرع مثل أبها/ابها </div>
         </div>
@@ -82,8 +82,29 @@ if(isset($_POST['signup-btn'])) {
             </div>
     </div>
 </form>
+<!--سكربت لإظهار العناصر باللون الأحمر في حالة عدم إدخال بيانات-->
+<script>
+      // Example starter JavaScript for disabling form submissions if there are invalid fields
+      (function() {
+        'use strict';
 
+        window.addEventListener('load', function() {
+          // Fetch all the forms we want to apply custom Bootstrap validation styles to
+          var forms = document.getElementsByClassName('needs-validation');
 
+          // Loop over them and prevent submission
+          var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      })();
+    </script>
 
 <!--نص إضافة معلومات لقاعدة البيانات-->
 <INSERT INTO `licenses` (`id`, `sequence`, `branch`, `region`, `date-of-shop-license`,
