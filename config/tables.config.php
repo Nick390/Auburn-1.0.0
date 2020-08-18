138ac615ce86a8849b9ad46a09568db39d938810
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-08-18 17:17:44 +0300

    Update more than one file
    *Update in tables.config.php to rename the database.
    *Update in footer.php with a new links to local js files.
    *Update in header.php with a new links to local css files.
    *Update in pages-footer.php with a new links to local js files.
    *Update in pages-header.php with a new links to local css files.
    *Update in index.php to change the user's direction when logging in.

--- config/tables.config.php
+++ config/tables.config.php
@@ -2,7 +2,7 @@
 #Connect to the database
 require_once '../includes/source/dp_connect.php';
 #Create a new Tables if not exist
-$CREATEDATABASE = "CREATE DATABASE IF NOT EXISTS `login`;";
+$CREATEDATABASE = "CREATE DATABASE IF NOT EXISTS `mydb`;";
 $DownloadHistory = "CREATE TABLE `login`.`downloadhistory` (
     `id` int(6) NOT NULL AUTO_INCREMENT,
     `jobNumber` varchar(11) NOT NULL,
@@ -22,7 +22,7 @@ $Groups = "";
 $Versions = "";
 
 $CommercialRecords = "";
-$Users = "CREATE TABLE IF NOT EXISTS `login`.`users` (
+$Users = "CREATE TABLE IF NOT EXISTS `mydb`.`users` (
     `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
     `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
     `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
--- includes/php/pages-header.php
+++ includes/php/pages-header.php
@@ -10,9 +10,9 @@ require_once '../includes/source/dp_connect.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>بوابة العلاقات الحكومية</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style/bootstrap.min.css">
     <link rel="stylesheet" href="/style/mybootstrap.css">
-    <script src="https://kit.fontawesome.com/03da71d2d8.js"></script>
+    <link rel="stylesheet" href="/style/css/all.min.css">
     <link rel="shortcut icon" type="image/png" href="/images/favicon.ico" />
 </head>
 <body>
