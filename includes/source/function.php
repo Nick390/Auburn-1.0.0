c5864e45fcf85620d4fcddaec680c287a9148e3a
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-08-25 15:23:31 +0300

    Update in function.php
    *Remove comment.
    *Add functions.

--- includes/source/function.php
+++ includes/source/function.php
@@ -1,5 +1,22 @@
 <?php
 
-/*function */
+    define('DBINFO', 'mysql:host=localhost;dbname=mydb');
+    define('DBUSER','root');
+    define('DBPASS','12345');
+
+    function fetchAll($query){
+        $con = new PDO(DBINFO, DBUSER, DBPASS);
+        $stmt = $con->query($query);
+        return $stmt->fetchAll();
+    }
+    function performQuery($query){
+        $con = new PDO(DBINFO, DBUSER, DBPASS);
+        $stmt = $con->prepare($query);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 
 ?>
