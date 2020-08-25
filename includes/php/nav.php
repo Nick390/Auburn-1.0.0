93f3cfb000aa7ba0125fc980b179f30ae5f73ae5
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-08-25 15:13:56 +0300

    Update in nav.php
    *Update navbar.
    *Add target for easy access.

--- includes/php/nav.php
+++ includes/php/nav.php
@@ -8,17 +8,30 @@
 <div class="d-flex" id="wrapper">
 
 <!-- Sidebar -->
-<div class="bg-light border-right" id="sidebar-wrapper">
-  <div class="sidebar-heading">Auburn </div>
+
+<div class="bg-light border-right navbar-light" id="sidebar-wrapper">
+  <div class="sidebar-heading"><a class="navbar-brand" href="/admin/dashboard.php">Auburn </a></div>
+  
   <div class="list-group list-group-flush">
-    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-tachometer-alt"></i> لوحة التحكم</a>
+    <a href="/admin/dashboard.php" class="list-group-item list-group-item-action bg-light"><i class="fas fa-tachometer-alt"></i> لوحة التحكم</a>
     <a href="#" class="list-group-item list-group-item-action bg-light"> <i class="fas fa-cogs"></i> الإعدادات</a>
     <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-clone"></i> الصفحات</a>
     <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-pencil-ruler"></i> الموضوع</a>
     <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-photo-video"></i> المعرض</a>
-    <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-puzzle-piece"></i> الإضافات</a>
+    <a class="list-group-item list-group-item-action bg-light" data-toggle="collapse" href="#dropdown-lvl1"><i class="fas fa-puzzle-piece"></i> الإضافات
+    <i class="fas fa-caret-down ico-margin"></i></a>
+						<!-- Dropdown level 1 -->
+						<div id="dropdown-lvl1" class="panel-collapse collapse">
+							<div class="panel-body">
+								<ul class="nav navbar-nav">
+									<li><a class="list-group-item list-group-item-action bg-light" href="/admin/submit-a-new-notification.php">إرسال إشعار جديد</a></li>
+                </ul>
+              </div>
+            </div>
     <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-id-card-alt"></i> الحساب</a>
     <a href="#" class="list-group-item list-group-item-action bg-light"><i class="fas fa-book"></i> دليل المستخدم</a>
+   
+
   </div>
 </div>
 <!-- /#sidebar-wrapper -->
@@ -41,7 +54,7 @@
           <a class="nav-link" href="dashboard.php">الرئيسية</a>
         </li>
         <li class="nav-item active">
-          <a class="nav-link" href="../pages/dashboard.php">عرض الموقع</a>
+          <a class="nav-link" href="../pages/dashboard.php" target="_blank">عرض الموقع</a>
         </li>
         <li class="nav-item active">
           <a class="nav-link" href="#"><i class="fas fa-adjust"></i></a>
@@ -53,4 +66,5 @@
       <a href="/index.php?logout"><button class="btn btn-outline-danger my-2 my-sm-0">تسجيل الخروج</button><a/>
     </div>
   </nav>
-  <div class="container-fluid">
+  <div class="container">
+  <div class="row m-2">
