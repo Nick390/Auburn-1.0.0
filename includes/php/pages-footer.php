cd8003e664d7e71cb50c1a609e413ddd9f8ac7b2
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-08-25 15:14:39 +0300

    Update in pages-footer.php
    *Remove footer.

--- includes/php/pages-footer.php
+++ includes/php/pages-footer.php
@@ -1,9 +1,5 @@
 <!-- Footer -->
-<footer class="footer position-sticky avbar-dark bg-dark">
-      <div class="container">
-        <div class="text-center">جميع الحقوق محفوظة</div>
-      </div>
-    </footer>
+
 <!-- Footer -->
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
