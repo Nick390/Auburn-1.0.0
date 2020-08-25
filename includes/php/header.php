0a72ef84d49f744bf69d06117162add77d136a4a
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-08-25 15:11:51 +0300

    Update in header.php
    *Remove link to mybootstrap.css.
    *Add css code to design the page.

--- includes/php/header.php
+++ includes/php/header.php
@@ -12,9 +12,42 @@ require_once '../includes/source/dp_connect.php';
     <title>بوابة العلاقات الحكومية</title>
     <link rel="stylesheet" href="/style/bootstrap.min.css">
     <link rel="stylesheet" href="/style/admin-sidebar.css">
-    <link rel="stylesheet" href="/style/mybootstrap.css">
     <link rel="stylesheet" href="/style/css/all.min.css">
     <link rel="shortcut icon" type="image/png" href="images/favicon.ico" />
+    <style>
+ *{
+   direction: rtl;
+   padding: 0px;
+   margin: 0px;
+   text-align: right;
+}
+.dropdown-item{
+    text-align: right;
+}
+.form-control-borderless {
+    border: none;
+}
+
+.form-control-borderless:hover, .form-control-borderless:active, .form-control-borderless:focus {
+    border: none;
+    outline: none;
+    box-shadow: none;
+}
+tr th, tr td {
+    text-align: center !important;
+}
+.text-center{
+    color: #fff;
+    text-align: center;
+    padding: 14px;
+}
+#upload-a-picture{
+    color: #212529;
+}
+.ico-margin{
+    margin-right: 7rem;
+}
+    </style>
 </head>
 <body>
  
