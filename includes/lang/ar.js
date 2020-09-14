2b9fe0c72cea0166e786da5f99bbdbdbd796b7bf
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-09-15 02:18:24 +0300

    Create more then one file
    *Create lang file To make translation file in one place.
    *Create ar.js system main language file.
    *Create functions.js To manage functions from one place.

---
+++ includes/lang/ar.js
@@ -0,0 +1,47 @@
+let langdata = {
+    "languages": {
+    //"AUBURN" : "أبورن" = {
+       // "Includes" : "يشمل" = {
+            //شريط التنقل للمشرف المميز
+            "ar": {
+            "strings": {
+                "controlpanel" : "لوحة التحكم",
+                "Settings" : "الإعدادات",
+                "Pages" : "الصفحات",
+                "Themes" : "الموضوع",
+                "Gallery" : "المعرض",
+                "Plugins" : "الإضافات",
+                "Posts" : "المنشورات",
+                "Users" : "المستخدمين",
+                "Account" : "الحساب",
+                "UserGuide" : "دليل المستخدم",
+                "Main" : "الرئيسية",
+                "ViewTheSite" : "عرض الموقع",
+                "LogOut" : "تسجيل الخروج"
+                }
+            },
+            "fr": {
+                "strings": {
+                    "controlpanel" : "لالالالا",
+                    "Settings" : "الإعدادات",
+                    "Pages" : "الصفحات",
+                    "Themes" : "الموضوع",
+                    "Gallery" : "المعرض",
+                    "Plugins" : "الإضافات",
+                    "Posts" : "المنشورات",
+                    "Users" : "المستخدمين",
+                    "Account" : "الحساب",
+                    "UserGuide" : "دليل المستخدم",
+                    "Main" : "الرئيسية",
+                    "ViewTheSite" : "عرض الموقع",
+                    "LogOut" : "تسجيل الخروج"
+                }
+            },
+            "de": {
+                "strings": {
+                    "btn-yes": "ja",
+                    "btn-no": "nein"
+                }
+            }
+        }
+    }
