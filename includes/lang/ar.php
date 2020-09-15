fadd9444c6d485d3a6cd3931c0d532c38b629e1d
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-09-15 15:31:48 +0300

    Create more then one file
    *Create ar.php for the Arabic language.
    *Create en.php for the English language.

---
+++ includes/lang/ar.php
@@ -0,0 +1,20 @@
+<?php
+	$lang = array(
+                //صفحة المشرف المميز
+                "Controlpanel" => "لوحة التحكم",
+                "Settings" => "الإعدادات",
+                "Pages" => "الصفحات",
+                "Themes" => "الموضوع",
+                "Gallery" => "المعرض",
+                "Plugins" => "الإضافات",
+                "Posts" => "المنشورات",
+                "Users" => "المستخدمين",
+                "Account" => "الحساب",
+                "UserGuide" => "دليل المستخدم",
+                "Main" => "الرئيسية",
+                "ViewTheSite" => "عرض الموقع",
+                "LogOut" => "تسجيل الخروج",
+                "lang_en" => "English",
+		        "lang_ar" => "العربية"
+                );
+?>
---
+++ includes/lang/en.php
@@ -0,0 +1,20 @@
+<?php
+	$lang = array(
+                //Super Admin page
+                "Controlpanel" => "Control Panel",
+                "Settings" => "Settings",
+                "Pages" => "Pages",
+                "Themes" => "Themes",
+                "Gallery" => "Gallery",
+                "Plugins" => "Plugins",
+                "Posts" => "Posts",
+                "Users" => "Users",
+                "Account" => "Account",
+                "UserGuide" => "User Guide",
+                "Main" => "Main",
+                "ViewTheSite" => "View Site",
+                "LogOut" => "LogOut",
+                "lang_en" => "English",
+		        "lang_ar" => "العربية"
+                );
+?>
