1c19c76329c306396acf3ff307d28830497a4848
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-10-01 04:06:28 +0300

    Create more then file
    *Create folder files to handle all files uploaded to server.
    *Create gallery.php to display all files that have been uploaded.
    *Create UploadMultipleFiles.php to upload files to the server.
    *Create AUBURN.png as a sample.

---
+++ admin/UploadMultipleFiles.php
@@ -0,0 +1,92 @@
+<?php include('../includes/php/header.php'); ?>
+<?php include('../includes/php/nav.php'); ?>
+<div class="container m-0 p-0">
+<div class="row m-0 p-0">
+        <div class="col-sm m-0 p-0">
+<h3><?php echo $lang['Page Title']?></h3>
+		<p><?php echo $lang['Page Info']?></p>
+    <form action="" method="POST" enctype="multipart/form-data">
+        <input type="file" name="userfile[]" value="" multiple="">
+        <button type="submit" name="submit" class="btn btn-primary"><?php echo $lang['UploadButton'] ?></button>
+    </form>
+    </div>
+        </div>
+        <div class="row m-0 p-0 mt-3">
+        <div class="col-sm m-0 p-0">
+<?php
+    $phpFileUploadErrors = array(
+    0 => $lang['There is no error, the file uploaded with success'],
+    1 => $lang['The uploaded file exceeds the upload_max_filesize directive in php.ini'],
+    2 => $lang['The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the html form'],
+    3 => $lang['The uploaded file was only partially uploaded'],
+    4 => $lang['No file was uploaded'],
+    6 => $lang['Missing a temporary folder'],
+    7 => $lang['Failed to  write file to disk'],
+    8 => $lang['A PHP extension stopped the file upload'],
+    );
+    if(isset($_FILES['userfile'])){
+        $file_array = reArrayFiles($_FILES['userfile']);
+        //pre_r($file_array);
+        for ($i=0; $i<count($file_array); $i++) { 
+            if($file_array[$i]['error']){
+?>
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <?php echo $file_array[$i]['name'].' - '.$phpFileUploadErrors[$file_array[$i]['error']]?>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+    </button>
+    </div>
+<?php
+            } else {
+                $extensions = array('jpg','png','gif','jpeg');
+                $file_ext = explode('.',$file_array[$i]['name']);
+                $file_ext = end($file_ext);
+                
+                if(!in_array($file_ext, $extensions)){
+    ?><div class="alert alert-danger alert-dismissible fade show" role="alert">
+    <?php echo "{$file_array[$i]['name']} - {$lang['Invalid file extension!']}"?>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+    </button>
+    </div>
+<?php
+                } else {
+                    move_uploaded_file($file_array[$i]['tmp_name'], "files/".$file_array[$i]['name']);
+    ?><div class="alert alert-success alert-dismissible fade show" role="alert">
+    <?php echo $file_array[$i]['name'].' - '.$phpFileUploadErrors[$file_array[$i]['error']]; ?>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+    </button>
+    </div>
+<?php
+                }
+            }
+        }
+    }
+    //convers $FILES array to cleaner array when uploading multiple files
+    function reArrayFiles( $file_post) {
+
+        $file_ary = array();
+        $file_count = count($file_post['name']);
+        $file_keys = array_keys($file_post);
+
+        for($i=0; $i<$file_count; $i++){
+            foreach($file_keys as $key){
+                $file_ary[$i] [$key] = $file_post[$key][$i];
+            }
+        }
+        
+        return $file_ary;
+    }
+    //Same as print_r() surrounded with <pre></pre> HTML tags for better array readability
+    function pre_r($array){
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
+?>
+
+</div>
+</div>
+</div>
+<?php include('../includes/php/footer.php'); ?>
---
+++ admin/gallery.php
@@ -0,0 +1,110 @@
+<?php include('../includes/php/header.php'); ?>
+<?php include('../includes/php/nav.php'); ?>
+<?php include('../includes/source/function.php'); ?>
+<div class="container">
+    <br/>
+	<div class="row justify-content-center">
+                        <div class="col-12 col-md-10 col-lg-8">
+                            <form class="card card-sm">
+                                <div class="card-body row no-gutters align-items-center">
+                                    <div class="col-auto">
+                                        <i class="fas fa-search h4 text-body mb-0"></i>
+                                    </div>
+                                    <!--end of col-->
+                                    <div class="col mr-3 ml-3">
+                                        <input type="search" placeholder="<?php echo $lang['Search For A File Using Its Name Or Format'] ?>" class="form-control search-input form-control-lg form-control-borderless" data-table="customers-list"/>
+                                    </div>
+                                    <!--end of col-->
+                                </div>
+                            </form>
+                        </div>
+    </div>
+                        <!--end of col-->
+    <br/>
+    <div class="row">
+        <div class="col">
+        <a href="/admin/UploadMultipleFiles.php"><button class="btn btn-sm btn-primary"><?php echo $lang['Upload New File Button'] ?></button></a>
+                    </div>
+                    </div>
+    <br/>
+</div>
+<br/>
+    
+<?php
+                $query = "SELECT * FROM `add_a_new_branch` order by `id`";
+                 if(count(fetchAll($query))>0)
+
+echo '<table class="table table-sm customers-list">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">رقم الفرع</th>
+          <th scope="col">الحي</th>
+          <th scope="col">المنطقة</th>
+          <th scope="col"></th>
+        </tr>
+      </thead>
+      <tbody>';
+
+if ($result = $conn->query($query)) {
+    while ($row = $result->fetch_assoc()) {
+        $field1name = $row["ID"];
+        $field2name = $row["BranchNumber"];
+        $field3name = $row["Neighborhood"];
+        $field4name = $row["Region"];
+
+        echo ' <tr>
+                  <td>'.$field1name.'</td> 
+                  <td>'.$field2name.'</td> 
+                  <td>'.$field3name.'</td> 
+                  <td>'.$field4name.'</td>
+                  <td><a href="#"><button class="btn btn-sm btn-secondary">عرض ملفات الفرع</button></a></td>
+                </tr>
+                ';
+    }
+    echo'</tbody>
+         </table>';
+}else{
+  echo '<div class="alert alert-danger" role="alert">يوجد مشكلة في قاعدة البيانات</div>';
+}
+?>
+    <script>
+        (function(document) {
+            'use strict';
+
+            var TableFilter = (function(myArray) {
+                var search_input;
+
+                function _onInputSearch(e) {
+                    search_input = e.target;
+                    var tables = document.getElementsByClassName(search_input.getAttribute('data-table'));
+                    myArray.forEach.call(tables, function(table) {
+                        myArray.forEach.call(table.tBodies, function(tbody) {
+                            myArray.forEach.call(tbody.rows, function(row) {
+                                var text_content = row.textContent.toLowerCase();
+                                var search_val = search_input.value.toLowerCase();
+                                row.style.display = text_content.indexOf(search_val) > -1 ? '' : 'none';
+                            });
+                        });
+                    });
+                }
+
+                return {
+                    init: function() {
+                        var inputs = document.getElementsByClassName('search-input');
+                        myArray.forEach.call(inputs, function(input) {
+                            input.oninput = _onInputSearch;
+                        });
+                    }
+                };
+            })(Array.prototype);
+
+            document.addEventListener('readystatechange', function() {
+                if (document.readyState === 'complete') {
+                    TableFilter.init();
+                }
+            });
+
+        })(document);
+    </script>
+<?php include('../includes/php/footer.php'); ?>
