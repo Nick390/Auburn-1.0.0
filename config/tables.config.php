011a55cde68ddad4483cdfe8bcbf2dbafff19e57
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-10-04 10:23:08 +0300

    Update tables.config.php
    *Update in tables.config.php.
    **Add user_date_created.

--- config/tables.config.php
+++ config/tables.config.php
@@ -50,16 +50,16 @@ $Groups = "";
 $Versions = "";
 
 $CommercialRecords = "";
-$Users = "CREATE TABLE IF NOT EXISTS `mydb`.`users` (
-    `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
-    `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
-    `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
-    `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique',
-    PRIMARY KEY (`user_id`),
-    `user_date_created` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s date and time created',
-    UNIQUE KEY `user_name` (`user_name`),
-    UNIQUE KEY `user_email` (`user_email`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data';";
+$Users = "CREATE TABLE `users` (
+   `user_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'auto incrementing user_id of each user, unique index',
+   `user_name` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s name, unique',
+   `user_password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s password in salted and hashed format',
+   `user_email` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s email, unique',
+   `user_date_created` varchar(64) COLLATE utf8_unicode_ci NOT NULL COMMENT 'user''s date and time created',
+   PRIMARY KEY (`user_id`),
+   UNIQUE KEY `user_name` (`user_name`),
+   UNIQUE KEY `user_email` (`user_email`)
+  ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='user data'";
   
 $ValidityOfLicenses = "";
 $BranchFilesAndUploadAndUpdate = "";
