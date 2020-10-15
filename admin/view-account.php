0c9276ffc615472591da90670b75384d0e4bf15d
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-10-15 15:23:49 +0300

    Create view-account.php
    **Create view-account.php.

---
+++ admin/view-account.php
@@ -0,0 +1,10 @@
+<?php include('../includes/php/header.php'); ?>
+<?php include('../includes/php/nav.php'); ?>
+<?php include_once '../includes/source/function.php';?>
+
+<?php echo $_SESSION['user_name']."</br>"?>
+<?php echo $_SESSION['user_email']."</br>"?>
+<?php echo $_SESSION['user_date_created']?>
+
+
+<?php include('../includes/php/footer.php'); ?>
