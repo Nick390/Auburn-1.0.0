d80145a6c263d4371e26fa4d682c913b3fd6e9e3
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-10-15 15:20:34 +0300

    Change create-new-password.php location & Rename it
    **Change create-new-password.php location to pages/form_create_new_password.php location.
    **Rename form create-new-password.php to form_create_new_password.php.

--- pages/form_create_new_password.php
+++ pages/form_create_new_password.php
@@ -1,4 +1,10 @@
-<?php /*<html lang="en">
+<?php
+require_once '../includes/source/dp_connect.php';
+
+$result = $conn->query("SELECT * FROM `settings`") or die($conn->error);
+$data = $result->fetch_assoc();
+?>
+<html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -41,8 +47,17 @@ body {
 .form-signin .form-control:focus {
   z-index: 2;
 }
-.form-signin input[type="email"] {
+.form-signin input[type="password"] {
   margin-bottom: -1px;
+}
+.password{
+margin-bottom: -1px;
+border-bottom-right-radius: 0;
+border-bottom-left-radius: 0;
+}
+.confirm-password{
+border-top-right-radius: 0;
+border-top-left-radius: 0;
 }
 .signupsuccess{
 
@@ -55,24 +70,24 @@ body {
             $validator = $_GET["validator"];
 
             if(empty($selector) || empty($validator)){
-                echo "<div class="alert alert-danger" role="alert">
+                echo '"<div class="alert alert-danger" role="alert">
                 لم نتمكن من التأكد من طلبك
-              </div>";
+              </div>"';
             } else {
-                if (ctype_xdigit($selector) !== false && (ctype_xdigit($validator) !== false){
-                    ?>
-                 <form class="form-signin" action="../includes/forgot-your-password.inc.php" method="post">
-                 <img class="mb-4" src="/images/logo.svg" alt="" width="72" height="72">
-                 <input type="hidden" name="selector" value="<?php echo $selector ?>">
-                 <input type="hidden" name="validator" value="<?php echo $validator ?>">
+                if (ctype_xdigit($selector) !== false && (ctype_xdigit($validator) !== false)){}
+            }?>
+                 <form class="form-signin" action="../pages/process_create_new_password.php" method="post">
+                 <img class="mb-4" src="<?php echo "{$data['website_login_icon']}"; ?>" alt="" width="72" height="72">
+                 <input type="hidden" name="selector" value="<?php echo $selector ;?>">
+                 <input type="hidden" name="validator" value="<?php echo $validator ;?>">
                  <h1 class="h3 mb-3 font-weight-normal">إعادة تعيين كلمة المرور</h1>
                  <p>من فضلك ادخل كلمة المرور الجديدة</p>
                  <label for="inputPassword" class="sr-only">كلمة المرور الجديدة</label>
-                 <input type="password" name="user-pass" id="inputPassword" class="form-control" placeholder="كلمة المرور" required="">
-                 <input type="password" name="user-pass-repeat" id="inputPassword" class="form-control" placeholder="تأكيد كلمة المرور" required="">
+                 <input type="password" name="user-pass" id="inputPassword" class="form-control password" placeholder="كلمة المرور" required>
+                 <input type="password" name="user-pass-repeat" id="inputPassword" class="form-control confirm-password" placeholder="تأكيد كلمة المرور" required>
+                 <br>
                  <button type="submit" name="reset-password-submit" class="btn btn-lg btn-primary btn-block">إعادة تعيين كلمة المرور</button>
                  <p class="mt-5 mb-3 text-muted">© 2019-2020</p>
                  </form>
 </body>
-</html>*/
-?>
+</html>
