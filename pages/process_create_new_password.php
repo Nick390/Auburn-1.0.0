5a254884ce477f1503ad965a93a75cd7d8421a7c
AlwaleedAlwabel <41131762+Nick390@users.noreply.github.com>
2020-10-15 15:16:39 +0300

    Change reset-password.inc.php location & Rename it
     **Change reset-password.inc.php location to pages/process_create_new_password.php location.
    **Rename form reset-password.inc.php to process_create_new_password.php.

--- pages/process_create_new_password.php
+++ pages/process_create_new_password.php
@@ -1,5 +1,5 @@
 <?php
-/*if (isset($_POST["reset-password-submit"])) {
+if (isset($_POST["reset-password-submit"])) {
 
     $selector = $_POST["selector"];
     $validator = $_POST["validator"];
@@ -21,25 +21,25 @@
     $sql = "SELECT * FROM pwdReset WHERE pwdResetSelector=? AND pwdResetExpires >= ?";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)){
-        echo "There was an error!";
+        echo "حصل خطأ عند محاولة التأكد من بيانات الرابط المستخدم مع بيانات قاعدة البيانات";
      exit();
     } else {
         mysqli_stmt_bind_param($stmt, "s", $selector, $selector);
         mysqli_stmt_execute($stmt);
 
         $result = mysqli_stmt_get_result($stmt);
-        if (!row = mysqli_fetch_assoc($result)) {
-            echo "You need to re_submit your reset request.";
+        if ($row = mysqli_fetch_assoc($result)) {
+            echo "يتوجب عليك إعادة تعيين كلمة المرور مرة أخرى";
         exit(); 
         } else {
           $tokenBin = hex2bin($validator);
           $tokenCheck = password_verify($tokenBin, $row["pwdResetToken"]);
           if ($tokenCheck === false) {
-            echo "You need to re_submit your reset request tow.";
+            echo "يتوجب عليك إعادة تعيين كلمة المرور مرة أخرىٍ";
             exit(); 
           } elseif ($tokenCheck === true){
               $tokenEmail = $row['pwdResetEmail'];
-              $sql = "SELECT * FROM users WHERE email=?;";
+              $sql = "SELECT * FROM users WHERE user_email=?;";
               $stmt = mysqli_stmt_init($conn);
               if(!mysqli_stmt_prepare($stmt, $sql)){
                   echo "There was an error!";
@@ -48,11 +48,11 @@
                   mysqli_stmt_bind_param($stmt, "s" , $tokenEmail);
                   mysqli_stmt_execute($stmt);
                   $result = mysqli_stmt_get_result($stmt);
-                  if (!row = mysqli_fetch_assoc($result)) {
+                  if ($row = mysqli_fetch_assoc($result)) {
                     echo "There was an eroor ! tow";
                     exit(); 
                     } else {
-                        $sql = "UPDATE users SET pwdUsers=? WHERE email=?";
+                        $sql = "UPDATE users SET user_password_hash=? WHERE user_email=?";
                         $stmt = mysqli_stmt_init($conn);
                         if(!mysqli_stmt_prepare($stmt, $sql)){
                             echo "There was an error!";
@@ -62,32 +62,33 @@
                             mysqli_stmt_bind_param($stmt, "ss" , $newPwdHash, $tokenEmail);
                             mysqli_stmt_execute($stmt);
 
-                            $sql = "UPDATE users SET pwdUsers=? WHERE email=?";
+                            $sql = "UPDATE users SET user_password_hash=? WHERE user_email=?";
                             $stmt = mysqli_stmt_init($conn);
                             if(!mysqli_stmt_prepare($stmt, $sql)){
-                                echo "There was an error!";
+                                echo "حدث خطأ عند محاولة القيام بعملية تشفير كلمة المرور وإضافتها إلى قاعدة البيانات";
                             exit();
                             } else {
                                 $newPwdHash = password_hash($password, PASSWORD_DEFAULT);
                                 mysqli_stmt_bind_param($stmt, "ss" , $newPwdHash, $tokenEmail);
                                 mysqli_stmt_execute($stmt);
 
-                                $sql ="DELETE FROM pwdReset WHERE pwdResetEmail=?";
+                                $sql ="DELETE FROM pwdreset WHERE pwdResetEmail=?";
                                 $stmt = mysqli_stmt_init($conn);
                                 if(!mysqli_stmt_prepare($stmt, $sql)){
-                                echo "There was an error!";
+                                echo "حصل خطأ عند محاولة القيام بعملية حذف بيانات محاولة إستعادة كلمة المرور";
                                 exit();
                                 mysqli_stmt_bind_param($stmt, "s", $tokenEmail);
                                 mysqli_stmt_execute($stmt);
-                                header("Location: /index.php?newpwd=passeordupdated");
+                                header("Location: /?newpwd=passeordupdated");
                                 }
                             }
                         }
                     }
-              }
-          }
+                }
+            }
+        }
     }
 } else {
-    header("Location: /index.php");
-}*/
+    header('Location: /');
+}
 ?>
